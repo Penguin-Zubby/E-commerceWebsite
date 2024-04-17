4bc3c9cda8bb0866ed07bb9df8db2628db510e71
Penguin-Zubby <140756585+Penguin-Zubby@users.noreply.github.com>
2024-04-17 15:34:03 +0100

    Added user authentication for login page i am about to create

--- allRoutes/loginRoute.js
+++ allRoutes/loginRoute.js
@@ -1,4 +1,44 @@
 const express = require("express");
 const router = express.Router();
 
+const userData = [
+  { username: "zubair", password: "123abc" },
+  { username: "john", password: "789xyz" },
+];
+
+function checkUserid(username, password) {
+  if (userData.find((user) => user.username === username)) {
+    if (userData.find((user) => user.password === password)) {
+      return true;
+    }
+  }
+}
+router.post("/", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (chechUserid(username, password)) {
+    req.sessipon.isAuthenticated = true;
+    res.redirect("/userHome");
+  } else {
+    req.session.flash = {
+      type: "error",
+      message: "Username or password is incorrect",
+    };
+    res.redirect("/login");
+  }
+});
+
+router.get("/", function (req, res) {
+  let userMessageType = null;
+  let userMessage = null;
+
+  if (req.session.flash) {
+    userMessageType = req.session.flash.type;
+    userMessage = req.session.flash.message;
+    req.session.flash = "";
+  }
+  res.render("login", { title: "Login", userMessageType, userMessage });
+});
+
 module.exports = router;
--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const session = require("express-session");
 const port = 3500;
+
+var indexRouter = require("./allRoutes/index");
+var loginRouter = require("./allRoutes/loginRoute");
+
+app.set("trust proxy", 1);
+app.use(
+  session({
+    secret: "flying penguin are awesome",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+//setting up view engine
+app.set("pages", path.join(_dirname, "pages"));
+app.set("pages engine", "hbs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(_dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/login", loginRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
