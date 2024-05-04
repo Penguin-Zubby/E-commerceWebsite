3ba9b44c9d3d6d92b0462caef9f471988dfb822a
Penguin-Zubby <140756585+Penguin-Zubby@users.noreply.github.com>
2024-05-04 19:42:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/user", userRouter);
+/*app.use("/user", userRouter);*/
 app.use("/login", loginRouter);
 app.use("/products", productRouter);
 app.use("/register", registerRouter);
